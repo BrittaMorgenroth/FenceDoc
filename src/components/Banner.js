bbd170ca9fb49dfb2b5289f4d656a1efc10fa924
BrittaMorgenroth <Britta.Morgenroth@gmail.com>
2022-09-20 15:56:55 +0200

    new code

---
+++ src/components/Banner.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './Banner.scss'
+
+export const Banner = () => {
+  return (
+    <div>Banner</div>
+  )
+}
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,32 +1,47 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
+import {FaBars, FaTimes} from 'react-icons/fa'
 
 import './NavBar.scss'
 
 function NavBar() {
+const[click, setClick] = useState(false)
+const handleClick = () => setClick(!click)
+
+
   return (
     <div className='header'>
     
         <Link to='/'><h1>LOGO</h1></Link>
-        <div className='navMenu'>
+        <div className={click ? 'navMenu active' : 'navMenu'}>
         <div className='navItem'>
-            <Link to='/'>Home long</Link>
+            <Link to='/'>Home</Link>
             <div className='dotContainer'>
             <div className='dot'></div>
             </div>
           </div>
           <div className='navItem'>
-            <Link to='/'>Home long very very long</Link>
+            <Link to='/'>About</Link>
             <div className='dotContainer'>
             <div className='dot'></div>
             </div>
           </div>
           <div className='navItem'>
-            <Link to='/'>Home long</Link>
+            <Link to='/'>Services</Link>
             <div className='dotContainer'>
             <div className='dot'></div>
             </div>
           </div>
+          <div className='navItem'>
+            <Link to='/'>Contact</Link>
+            <div className='dotContainer'>
+            <div className='dot'></div>
+            </div>
+          </div>
+
+          </div>
+          <div className='hamburger' onClick={handleClick}>
+            {click ? (<FaTimes/>) : (<FaBars />)}
 
           </div>
     </div>
