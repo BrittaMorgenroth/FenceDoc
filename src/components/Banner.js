56013dc87d02be346bb867547918036442c03a5b
BrittaMorgenroth <Britta.Morgenroth@gmail.com>
2022-09-22 18:15:59 +0200

    banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,29 @@
 import React from 'react'
 
+import {Link} from 'react-router-dom'
+
 import './Banner.scss'
 
 export const Banner = () => {
   return (
-    <div>Banner</div>
+    <div className='hero'>
+
+      <div className='content'>
+        <h2>We Do Fences</h2>
+        <h1>
+        WE WILL BUILD YOUR DREAM
+        </h1>
+        <div className='lineBreak'>
+          
+        </div>
+        <p>Sample text. Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices.</p>
+        <div>
+        <Link to='/'><button className='btn'>Call Us Now</button></Link>
+        <Link to='/'><button className='btn2'>About Us</button></Link>
+        </div>
+      </div>
+      
+      
+    </div>
   )
 }
